7c7b58fbb77e6f05c1f1c9ce6ddb756c25e8ed6c
Shajith Chacko <shajith@Shajith.local>
2010-12-01 16:21:24 -0800

    Checks the "pre-commit.checks" git config variable to determine which checks to run. Picks a default set if that variable is not found or empty.
    Configure like this:
    
    [pre-commit]
            checks = "white_space, console_log, debugger, tabs, closure_syntax_check"

--- pre-commit.rb
+++ pre-commit.rb
@@ -3,58 +3,74 @@
 
 
 class Utils
+
   def self.staged_files(*dirs)
     @staged_files ||= {}
     @staged_files[dirs.join(' ')] ||= `git diff --cached --name-only #{dirs.join(' ')} | xargs`.chomp
   end
+
+
 end
 
-WhiteSpace = lambda { system("sh ./.git/hooks/whitespace") }
+class PreCommit
 
-ConsoleLog = lambda { 
-  if (args = Utils.staged_files('public/javascripts')).size > 0
-    !system("git grep -n \"console\.log\" #{args}")
-  else
-    true
+  WhiteSpace = lambda { system("sh ./.git/hooks/whitespace") }
+
+  ConsoleLog = lambda {
+    if (args = Utils.staged_files('public/javascripts')).size > 0
+      !system("git grep -n \"console\.log\" #{args}")
+    else
+      true
+    end
+  }
+
+  Debugger = lambda {
+    if (args = Utils.staged_files('app/', 'lib/', 'script/', 'vendor/')).size > 0
+      !system("git grep debugger #{args}")
+    else
+      true
+    end
+  }
+
+  Tabs = lambda {
+    if (files = Utils.staged_files('*')).size > 0
+      !system("grep -PnH '^\t' #{files}")
+    else
+      true
+    end
+  }
+
+  ClosureSyntaxCheck = lambda {
+    compiler = "test/javascript/lib/compiler.jar"
+
+    if (args = Utils.staged_files('public/javascripts')).size > 0
+      js_args = args.map {|arg| "--js #{arg}"}.join(' ')
+      system("java -jar test/javascript/lib/compiler.jar #{js_args} --js_output_file /tmp/jammit.js")
+    else
+      true
+    end
+  }
+
+  Checks = {
+    :white_space          => WhiteSpace,
+    :console_log          => ConsoleLog,
+    :debugger             => Debugger,
+    :tabs                 => Tabs,
+    :closure_syntax_check => ClosureSyntaxCheck
+  }
+
+  def self.checks_to_run
+    checks_to_run = `git config pre-commit.checks`.chomp.split(/,\s*/).map(&:to_sym)
+
+    if checks_to_run.empty?
+      Checks.values_at(:white_space, :console_log, :debugger, :tabs, :closure_syntax_check)
+    else
+      Checks.values_at(*checks_to_run)
+    end.compact
   end
-}
+end
 
-Debugger = lambda {
-  if (args = Utils.staged_files('app/', 'lib/', 'script/', 'vendor/')).size > 0
-    !system("git grep debugger #{args}")
-  else
-    true
-  end
-}
-
-Tabs = lambda { 
-  if (files = Utils.staged_files('*')).size > 0
-    !system("grep -PnH '^\t' #{files}")
-  else
-    true
-  end
-}
-
-ClosureSyntaxCheck = lambda {
-  compiler = "test/javascript/lib/compiler.jar"
-
-  if (args = Utils.staged_files('public/javascripts')).size > 0
-    js_args = args.map {|arg| "--js #{arg}"}.join(' ')
-    system("java -jar test/javascript/lib/compiler.jar #{js_args} --js_output_file /tmp/jammit.js")
-  else
-    true
-  end
-}
-
-checks = [
-          WhiteSpace, 
-          ConsoleLog, 
-          Debugger, 
-          Tabs
-          #ClosureSyntaxCheck
-         ]
-
-exit_status = checks.inject(true) do |acc, cmd|
+exit_status = PreCommit.checks_to_run.inject(true) do |acc, cmd|
   acc = cmd.call && acc
 end
 
