ab683cf94be35d7d3efd99ee759f90d79c62ca56
Shajith Chacko <shajith@Shajith.local>
2010-12-15 00:19:51 -0800

    Using a diff-filter option to not use deleted files in checks.

--- lib/pre-commit/utils.rb
+++ lib/pre-commit/utils.rb
@@ -2,7 +2,7 @@ class Utils
 
   def self.staged_files(*dirs)
     @staged_files ||= {}
-    @staged_files[dirs.join(' ')] ||= `git diff --cached --name-only #{dirs.join(' ')} | xargs`.chomp
+    @staged_files[dirs.join(' ')] ||= `git diff --cached --name-only --diff-filter=ACM #{dirs.join(' ')} | xargs`.chomp
   end
 
   def self.new_files(*dirs)
