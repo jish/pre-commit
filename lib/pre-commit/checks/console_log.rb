8e03eb186dc9e7830571d4fbaab2ee887da43aaa
Josh Lubaway <josh.lubaway@gmail.com>
2010-12-19 19:41:38 -0800

    Recording an error message on ConsoleLog
    The old flow was to "shell out" to grep, then let it throw
    standard output on the screen whenever it ran. We want to change
    this so we have more control over it, and so our test output is
    not filled with grep output.

--- lib/pre-commit/checks/console_log.rb
+++ lib/pre-commit/checks/console_log.rb
@@ -1,9 +1,23 @@
 class ConsoleLog
 
-  attr_accessor :staged_files
+  attr_accessor :staged_files, :error_message
 
   def run
-    !system("grep 'console.log' #{staged_files}")
+    if detected_bad_code?
+      @error_message = "pre-commit: console.log found:\n"
+      @error_message += instances_of_console_log_violations
+      false
+    else
+      true
+    end
+  end
+
+  def detected_bad_code?
+    system("grep -q 'console.log' #{staged_files}")
+  end
+
+  def instances_of_console_log_violations
+    `grep -nH 'console.log' #{staged_files}`
   end
 
 end
--- test/unit/console_log_test.rb
+++ test/unit/console_log_test.rb
@@ -3,7 +3,6 @@ require 'pre-commit/checks/console_log'
 
 class ConsoleLogTest < MiniTest::Unit::TestCase
 
-  # TODO silence the output of this test
   def test_should_detect_an_extraneous_console_log_statement
     check = ConsoleLog.new
     check.staged_files = test_filename('console_log.js')
@@ -14,6 +13,16 @@ class ConsoleLogTest < MiniTest::Unit::TestCase
     check = ConsoleLog.new
     check.staged_files = test_filename('valid_file.js')
     assert check.run, 'A file with no `console.log` statements should pass'
+  end
+
+  def test_error_message_should_contain_an_error_message_when_console_log_is_found
+    check = ConsoleLog.new
+    check.staged_files = test_filename('console_log.js')
+    assert !check.run, 'We should prevent a `console.log` from being committed'
+
+    assert_match(/pre-commit: console.log found:/, check.error_message)
+    assert_match(/console_log.js/, check.error_message)
+    assert_match(/console.log/, check.error_message)
   end
 
 end
