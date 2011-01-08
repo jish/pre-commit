aacd03db98a68d4e3fda3351c2c1ba3c55e3b3cd
Josh Lubaway <josh.lubaway@gmail.com>
2011-01-08 15:44:39 -0800

    Adding a sample runner class.
    In order to make the runner more testable, we would like to
    encapsulate its logic into a class. This allows us to run tests
    against its functionality.
    
    The driving factor behind this change currently is the display of an
    error message when a check fails.
    
    The organization of the ConsoleLog check requires an error message
    to be output in the case of a failure.
    
    This change was made in the ConsoleLog check in the first place to
    make testing more pleasant. The old version spit out an error message
    immediately upon failure, which is fine at runtime. However, when
    testing error cases, we don't want to clutter the test output with
    user error messages.

---
+++ lib/pre-commit/runner.rb
@@ -0,0 +1,19 @@
+class Runner
+
+  def run
+    checks_to_run = PreCommit.checks_to_run
+
+    all_passed = checks_to_run.inject(true) do |current_status, check|
+      passed = check.call
+
+      if !passed && check.respond_to?(:error_message)
+        puts check.error_message
+      end
+
+      check && current_status
+    end
+
+    exit(all_passed ? 0 : 1)
+  end
+
+end
---
+++ test/unit/runner_test.rb
@@ -0,0 +1,9 @@
+require 'minitest/autorun'
+
+class RunnerTest < MiniTest::Unit::TestCase
+
+  def test_should_display_error_messages_for_failing_checks
+    runner = Runner.new
+  end
+
+end
