b23d993d516ad4c38d1324939a7cb48e3132ffc9
Josh Lubaway <josh.lubaway@gmail.com>
2011-05-21 14:20:46 -0700

    Adding a js_check_test.

--- test/unit/jshint_check_test.rb
+++ test/unit/jshint_check_test.rb
@@ -3,28 +3,10 @@ require 'pre-commit/checks/jshint_check'
 
 class JshintCheckTest < MiniTest::Unit::TestCase
 
-  def test_should_not_run_if_staged_files_is_empty
-    check = PreCommit::JshintCheck.new
-
-    assert check.should_run?(['foo.js'])
-
-    assert !check.should_run?([])
-  end
-
   def test_should_know_where_the_jshint_source_code_is
     check = PreCommit::JshintCheck.new
     expected = File.join(PreCommit.root, 'lib/support/jshint/jshint.js')
     assert_equal(expected, check.linter_src)
-  end
-
-  def test_should_reject_staged_files_that_are_not_js_files
-    check = PreCommit::JshintCheck.new
-    assert_equal(['bar.js'], check.reject_non_js(['foo.rb', 'bar.js']))
-  end
-
-  def test_reject_staged_files_should_not_blow_up_with_a_list_of_one_file
-    check = PreCommit::JshintCheck.new
-    assert_equal(['bar.js'], check.reject_non_js(['bar.js']))
   end
 
   def test_should_display_errors
--- test/unit/jslint_check_test.rb
+++ test/unit/jslint_check_test.rb
@@ -1,18 +1,7 @@
 require 'minitest/autorun'
 require 'pre-commit/checks/jslint_check'
 
-# TODO remove this #ugly
-require 'pre-commit/checks'
-
 class JslintCheckTest < MiniTest::Unit::TestCase
-
-  def test_should_not_run_if_staged_files_is_empty
-    check = PreCommit::JslintCheck.new
-
-    assert check.should_run?(['foo.js'])
-
-    assert !check.should_run?([])
-  end
 
   def test_should_run_all_by_default
     check = PreCommit::JslintCheck.new
