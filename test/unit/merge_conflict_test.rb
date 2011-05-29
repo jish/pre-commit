da7866ee5364e6d029e46015ec078152be0e5515
Josh Lubaway <josh.lubaway@gmail.com>
2011-05-28 23:00:30 -0700

    Silencing the output of the merge conflict test.

--- test/unit/merge_conflict_test.rb
+++ test/unit/merge_conflict_test.rb
@@ -3,17 +3,16 @@ require 'pre-commit/checks/merge_conflict'
 
 class MergeConflictTest < MiniTest::Unit::TestCase
 
-  # TODO silence the output of this test
   def test_should_detect_a_merge_conflict
     check = MergeConflict.new
     check.staged_files = test_filename('merge_conflict.rb')
-    assert !check.run, 'We should prevent a merge conflict from being committed'
+    assert check.detected_bad_code?, 'We should prevent a merge conflict from being committed'
   end
 
   def test_should_pass_a_file_with_no_merge_conflicts
     check = MergeConflict.new
     check.staged_files = test_filename('valid_file.rb')
-    assert check.run, 'A file with no merge conflicts should pass'
+    assert !check.detected_bad_code?, 'A file with no merge conflicts should pass'
   end
 
 end
