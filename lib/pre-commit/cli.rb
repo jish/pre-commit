1b852aea46459370926bee5279de70af9dad1a0b
Josh Lubaway <josh.lubaway@gmail.com>
2012-07-09 21:34:46 -0700

    Giving the option to overwrite hook during installation.
    Previously we were being extra safe and just aborting if we detected
    an existing git pre-commit hook. Now we ask the user if they would
    like use to replace their existing pre-commit hook.
    
    As part of this commit a `PreCommit::Cli` class was created along
    with some basic test cases to test user responses.

---
+++ lib/pre-commit/cli.rb
@@ -0,0 +1,42 @@
+require 'fileutils'
+require 'pre-commit/base'
+
+class PreCommit
+  class Cli
+
+    PRE_COMMIT_HOOK_PATH = '.git/hooks/pre-commit'
+
+    def answered_yes?(answer)
+      answer =~ /y\n/i || answer == "\n"
+    end
+
+    def install
+      if File.exists?(PRE_COMMIT_HOOK_PATH)
+        ask_to_overwrite
+      end
+
+      install_pre_commit_hook
+    end
+
+    def ask_to_overwrite
+      puts "pre-commit: WARNING There is already a pre-commit hook installed in this git repo."
+      print "Would you like to overwrite it? [Yn] "
+      answer = $stdin.gets
+
+      if answered_yes?(answer)
+        FileUtils.rm(PRE_COMMIT_HOOK_PATH)
+      else
+        puts "Not overwriting existing hook: #{PRE_COMMIT_HOOK_PATH}"
+        puts
+        exit(1)
+      end
+    end
+
+    def install_pre_commit_hook
+      hook = File.join(PreCommit.root, 'templates', 'pre-commit-hook')
+      FileUtils.cp(hook, PRE_COMMIT_HOOK_PATH)
+      FileUtils.chmod(0755, PRE_COMMIT_HOOK_PATH)
+    end
+
+  end
+end
