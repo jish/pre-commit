25fe06392ed385bd74b879e2a6f1cb2dc3c6b89b
grosser <grosser.michael@gmail.com>
2013-07-04 15:15:23 -0700

    simplify tabs check

--- lib/pre-commit/checks/tabs_check.rb
+++ lib/pre-commit/checks/tabs_check.rb
@@ -5,16 +5,10 @@ module PreCommit
     LEADING_TAB_PATTERN = '^ *\t'
 
     def self.run(staged_files)
-      return if staged_files.empty? || !detected_bad_code?(staged_files)
-      "detected tab before initial space:\n#{violations(staged_files)}"
-    end
-
-    def self.detected_bad_code?(staged_files)
-      system("#{Utils.grep} -q '#{LEADING_TAB_PATTERN}' #{staged_files.join(" ")}")
-    end
-
-    def self.violations(staged_files)
-      `#{Utils.grep} '#{LEADING_TAB_PATTERN}' #{staged_files.join(" ")}`
+      return if staged_files.empty?
+      errors = `#{Utils.grep} '#{LEADING_TAB_PATTERN}' #{staged_files.join(" ")}`
+      return unless $?.success?
+      "detected tab before initial space:\n#{errors}"
     end
   end
 end
