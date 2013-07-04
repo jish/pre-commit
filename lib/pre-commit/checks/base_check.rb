bd09f0874706f2bfb95bd5d49b8c7a4f9f17a00a
grosser <grosser.michael@gmail.com>
2013-07-04 11:11:27 -0700

    do not operate on removed files

--- lib/pre-commit/checks/base_check.rb
+++ lib/pre-commit/checks/base_check.rb
@@ -3,7 +3,8 @@ require 'pre-commit/utils'
 module PreCommit
   class BaseCheck
     def self.call(quiet=false)
-      error = run(Utils.staged_files(".").split(" "))
+      staged_files = Utils.staged_files(".").split(" ").select { |f| File.exist?(f) }
+      error = run(staged_files)
       puts "#{error}\npre-commit: You can bypass this check using `git commit -n`" if error && !quiet
       !error
     end
