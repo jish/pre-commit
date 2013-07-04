27e969851be121996f07e04d83fab9d86eb1ce92
grosser <grosser.michael@gmail.com>
2013-07-04 14:22:07 -0700

    convert rspec to base

--- lib/pre-commit/checks/rspec_focus_check.rb
+++ lib/pre-commit/checks/rspec_focus_check.rb
@@ -1,54 +1,12 @@
+require 'pre-commit/checks/base_check'
+
 module PreCommit
-  class RSpecFocusCheck
-
-    attr_accessor :staged_files, :error_message, :grep_command, :spec_files
-
-    def self.call(quiet=false)
-      dirs = ['spec/'].reject {|d| !File.exists?(d)}
-      check = new
-      check.staged_files = Utils.staged_files(*dirs)
-
-      result = check.run
-      if !quiet && !result
-        $stderr.puts check.error_message
-        $stderr.puts
-        check.print_dash_n_reminder
-      end
-      result
+  class RSpecFocusCheck < BaseCheck
+    def self.run(staged_files)
+      staged_files = staged_files.grep(/_spec\.rb$/)
+      return if staged_files.empty?
+      result = `grep -nH ':focus' #{staged_files.join(" ")}`.strip
+      ":focus found in specs:\n#{result}" if $?.success?
     end
-
-    def run
-      return true if staged_files.empty?
-
-      if detected_bad_code?
-        @error_message = "pre-commit: :focus in spec(s) found:\n"
-        @error_message += instances_of_rspec_focus_violations
-        false
-      else
-        true
-      end
-    end
-
-    def detected_bad_code?
-      spec_files = staged_files.split(' ').select do |file|
-        file =~ /_spec\.rb$/
-      end
-
-      return false if spec_files.empty?
-
-      diff = `git diff --cached -G:focus #{spec_files.join(" ")}`
-      !!(diff =~ /[\W\s]:focus[\W\s]/)
-    end
-
-    def instances_of_rspec_focus_violations
-      cmd = grep_command || "git grep"
-      `#{cmd} -nHw ':focus' #{staged_files}`
-    end
-
-    def print_dash_n_reminder
-      $stderr.puts 'You can bypass this check using `git commit -n`'
-      $stderr.puts
-    end
-
   end
 end
--- test/unit/rspec_focus_check_test.rb
+++ test/unit/rspec_focus_check_test.rb
@@ -1,31 +1,22 @@
 require 'minitest_helper'
 require 'pre-commit/checks/rspec_focus_check'
 
-class RSpecFocusCheckTest < MiniTest::Unit::TestCase
+describe PreCommit::RSpecFocusCheck do
+  let(:check){ PreCommit::RSpecFocusCheck }
 
-  def test_should_detect_a_focus_symbol
-    check = PreCommit::RSpecFocusCheck.new
-    check.grep_command = "grep"
-    check.staged_files = test_filename('bad_spec.rb')
-    assert !check.instances_of_rspec_focus_violations.empty?
+  it "succeeds if nothing changed" do
+    check.run([]).must_equal nil
   end
 
-  def test_should_pass_a_file_with_no_focus_symbol
-    check = PreCommit::RSpecFocusCheck.new
-    check.grep_command = "grep"
-    check.staged_files = test_filename('good_spec.rb')
-    assert check.instances_of_rspec_focus_violations.empty?
+  it "succeeds on non-specs" do
+    check.run([test_filename('bad-spec.rb')]).must_equal nil
   end
 
-  def test_error_message_should_contain_an_error_message_when_a_focus_symbol_is_found
-    check = PreCommit::RSpecFocusCheck.new
-    def check.detected_bad_code?; true; end
-    check.grep_command = "grep"
-    check.staged_files = test_filename('bad_spec.rb')
-    check.run
-
-    assert_match(/pre-commit: :focus in spec\(s\) found:/, check.error_message)
-    assert_match(/bad_spec.rb/, check.error_message)
+  it "succeeds if only good changes" do
+    check.run([test_filename('good_spec.rb')]).must_equal nil
   end
 
+  it "fails if file contains pry" do
+    check.run([test_filename('bad_spec.rb')]).must_equal ":focus found in specs:\ntest/files/bad_spec.rb:2:  context \"functionality\", :focus do"
+  end
 end
