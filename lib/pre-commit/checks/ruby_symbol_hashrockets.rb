425ea90540beeca800548d04a92160eac44ce809
grosser <grosser.michael@gmail.com>
2013-07-04 15:13:38 -0700

    convert symbol hashrockets to base

--- lib/pre-commit/checks/ruby_symbol_hashrockets.rb
+++ lib/pre-commit/checks/ruby_symbol_hashrockets.rb
@@ -1,55 +1,15 @@
 require 'pre-commit/utils'
+require 'pre-commit/checks/base_check'
 
 module PreCommit
-  class RubySymbolHashrockets
-
-    attr_accessor :staged_files, :error_message
-
+  class RubySymbolHashrockets < BaseCheck
     HASHROCKET_PATTERN = '[^:](:{1}(?:\$|@|@@|[_A-Za-z])?\w*[=!?]?\s*=>\s*)'
 
-    def self.call
-      check = new
-      check.staged_files = Utils.staged_files('*')
-      result = check.run
-
-      if !result
-        $stderr.puts check.error_message
-        $stderr.puts
-        $stderr.puts 'pre-commit: You can bypass this check using `git commit -n`'
-        $stderr.puts
-      end
-
-      result
+    def self.run(staged_files)
+      return if staged_files.empty?
+      lines = `#{Utils.grep} '#{HASHROCKET_PATTERN}' #{staged_files.join(" ")}`
+      return unless $?.success?
+      "detected :symbol => value hashrocket:\n#{lines}"
     end
-
-    def run
-      # There is nothing to check
-      return true if staged_files.empty?
-
-      if detected_bad_code?
-        @error_message = "pre-commit: detected :symbol => value hashrocket:\n"
-        @error_message += violations[:lines]
-
-        @passed = false
-      else
-        @passed = true
-      end
-
-      @passed
-    end
-
-    def detected_bad_code?
-      violations[:success]
-    end
-
-    def violations
-      @violations ||= begin
-        lines = `#{Utils.grep} '#{HASHROCKET_PATTERN}' #{staged_files}`
-        success = $?.exitstatus == 0
-
-        { :lines => lines, :success => success}
-      end
-    end
-
   end
 end
--- test/unit/ruby_symbol_hashrockets_test.rb
+++ test/unit/ruby_symbol_hashrockets_test.rb
@@ -1,42 +1,21 @@
 require 'minitest_helper'
 require 'pre-commit/checks/ruby_symbol_hashrockets'
 
-class RubySymbolHashrocketsTest < MiniTest::Unit::TestCase
+describe PreCommit::RubySymbolHashrockets do
+  let(:check){ PreCommit::RubySymbolHashrockets }
 
-  def test_should_detect_a_symbol_hashrocket
-    check = PreCommit::RubySymbolHashrockets.new
-    check.staged_files = test_filename('wrong_hashrockets.rb')
-    assert check.detected_bad_code?, 'should detect symbol hashrocket'
+  it "succeeds if nothing changed" do
+    check.run([]).must_equal nil
   end
 
-  def test_should_detect_all_symbol_hashrockets
-    check = PreCommit::RubySymbolHashrockets.new
-    check.staged_files = test_filename('wrong_hashrockets.rb')
-    assert_equal 8, check.violations[:lines].split("\n").size
+  it "succeeds with valid" do
+    check.run([test_filename('valid_hashrockets.rb')]).must_equal nil
   end
 
-  def test_does_not_detect_bad_code_in_a_valid_file
-    check = PreCommit::RubySymbolHashrockets.new
-    check.staged_files = test_filename('valid_hashrockets.rb')
-    assert !check.detected_bad_code?, 'should pass valid hashrocket'
-  end
-
-  def test_should_run_with_valid_file
-    check = PreCommit::RubySymbolHashrockets.new
-    check.staged_files = test_filename('valid_hashrockets.rb')
-    assert check.run, 'should run valid hashrocket '
-  end
-
-  def test_check_should_pass_if_staged_file_list_is_empty
-    check = PreCommit::RubySymbolHashrockets.new
-    check.staged_files = []
-    assert check.run
-  end
-
-  def test_guilty_file_in_error_message
-    check = PreCommit::RubySymbolHashrockets.new
-    check.staged_files = test_filename('wrong_hashrockets.rb')
-    check.run
-    assert_match(/wrong_hashrockets.rb/, check.error_message)
+  it "fails with invalid" do
+    result = check.run([test_filename('wrong_hashrockets.rb')])
+    result.must_include "detected :symbol => value hashrocket:\n"
+    result.must_include "test/files/wrong_hashrockets.rb:3:gem 'foo', :ref => 'v2.6.0'"
+    result.lines.to_a.size.must_equal 9
   end
 end
