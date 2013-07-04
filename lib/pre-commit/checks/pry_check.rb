01a7f2ebe2c9463701097388da6c2c5e01e3f815
grosser <grosser.michael@gmail.com>
2013-07-04 10:26:40 -0700

    convert pry to base

--- lib/pre-commit/checks/pry_check.rb
+++ lib/pre-commit/checks/pry_check.rb
@@ -1,47 +1,11 @@
+require 'pre-commit/checks/base_check'
+
 module PreCommit
-  class PryCheck
-
-    attr_accessor :staged_files, :error_message, :grep_command
-
-    def self.call(quiet=false)
-      dirs = %w(app/ lib/ script/ vendor/ test/ spec/).reject {|d| !File.exists?(d)}
-      check = new
-      check.staged_files = Utils.staged_files(*dirs)
-
-      result = check.run
-      if !quiet && !result
-        $stderr.puts check.error_message
-        $stderr.puts
-        check.print_dash_n_reminder
-      end
-      result
+  class PryCheck < BaseCheck
+    def self.run(staged_files)
+      return if staged_files.empty?
+      result = `grep -nH binding.pry #{staged_files.join(" ")}`.strip
+      "binding.pry found:\n#{result}" if $?.success?
     end
-
-    def run
-      return true if staged_files.empty?
-
-      if detected_bad_code?
-        @error_message = "pre-commit: binding.pry statement(s) found:\n"
-        @error_message += instances_of_pry_violations
-        false
-      else
-        true
-      end
-    end
-
-    def detected_bad_code?
-      !system("git diff --cached -S binding.pry --quiet --exit-code")
-    end
-
-    def instances_of_pry_violations
-      cmd = grep_command || "git grep"
-      `#{cmd} -nH "binding\.pry" #{staged_files}`
-    end
-
-    def print_dash_n_reminder
-      $stderr.puts 'You can bypass this check using `git commit -n`'
-      $stderr.puts
-    end
-
   end
 end
--- test/unit/pry_check_test.rb
+++ test/unit/pry_check_test.rb
@@ -1,31 +1,18 @@
 require 'minitest_helper'
 require 'pre-commit/checks/pry_check'
 
-class PryCheckTest < MiniTest::Unit::TestCase
+describe PreCommit::PryCheck do
+  let(:check){ PreCommit::PryCheck }
 
-  def test_should_detect_a_binding_pry_statement
-    check = PreCommit::PryCheck.new
-    check.grep_command = "grep"
-    check.staged_files = test_filename('pry_file.rb')
-    assert !check.instances_of_pry_violations.empty?
+  it "succeeds if nothing changed" do
+    check.run([]).must_equal nil
   end
 
-  def test_should_pass_a_file_with_no_binding_pry_statement
-    check = PreCommit::PryCheck.new
-    check.grep_command = "grep"
-    check.staged_files = test_filename('valid_file.rb')
-    assert check.instances_of_pry_violations.empty?
+  it "succeeds if only good changes" do
+    check.run([test_filename('valid_file.rb')]).must_equal nil
   end
 
-  def test_error_message_should_contain_an_error_message_when_a_binding_pry_statement_is_found
-    check = PreCommit::PryCheck.new
-    def check.detected_bad_code?; true; end
-    check.grep_command = "grep"
-    check.staged_files = test_filename('pry_file.rb')
-    check.run
-
-    assert_match(/pre-commit: binding.pry statement\(s\) found:/, check.error_message)
-    assert_match(/pry_file.rb/, check.error_message)
+  it "fails if file contains pry" do
+    check.run([test_filename('pry_file.rb')]).must_equal "binding.pry found:\ntest/files/pry_file.rb:3:    binding.pry"
   end
-
 end
