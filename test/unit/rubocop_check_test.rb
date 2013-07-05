48be6eb5694398bfdba4ecf74e5c5ab40057177a
grosser <grosser.michael@gmail.com>
2013-07-04 18:56:48 -0700

    fix rubocop again

--- test/unit/rubocop_check_test.rb
+++ test/unit/rubocop_check_test.rb
@@ -17,7 +17,7 @@ else
 
     it "fails if file contains errors" do
       # rubinius finds only 1 offense, all others find 2
-      check.run([test_filename('merge_conflict.rb')]).must_match /1 file inspected, \e\[31m[12] offences detected\e\[0m/
+      check.run([test_filename('merge_conflict.rb')]).must_match /1 file inspected, \e\[31m[12] offences? detected\e\[0m/
     end
   end
 end
