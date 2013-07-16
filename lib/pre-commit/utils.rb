236ff50d2974bbfa0705092d7388bdd77221ac4f
Benjamin Quorning <bquorning@zendesk.com>
2013-07-16 23:31:06 +0200

    Utils.staged_files is always called with (".")

--- lib/pre-commit/utils.rb
+++ lib/pre-commit/utils.rb
@@ -1,20 +1,13 @@
 module PreCommit
   class Utils
 
-    def self.staged_files(*dirs)
-      dirs = reject_missing(dirs)
-
-      @staged_files ||= {}
-      @staged_files[dirs.join(' ')] ||= `git diff --cached --name-only --diff-filter=ACM #{dirs.join(' ')} | xargs`.chomp
+    def self.staged_files
+      @staged_files ||= `git diff --cached --name-only --diff-filter=ACM | xargs`.chomp
     end
 
     def self.new_files(*dirs)
       @new_files ||= {}
       @new_files[dirs.join(' ')] ||= `git status --short #{dirs.join(' ')} | grep ^A | xargs`.chomp.split("A ").join(" ")
-    end
-
-    def self.reject_missing(dirs)
-      dirs.reject { |dir| !File.exist?(dir) }
     end
 
     def self.grep
