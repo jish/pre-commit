45ed702e36b3b4e49346db9a3f2c5f96e1bbec53
Benjamin Quorning <bquorning@zendesk.com>
2013-07-16 23:41:20 +0200

    Utils.new_files is not used anymore

--- lib/pre-commit/utils.rb
+++ lib/pre-commit/utils.rb
@@ -5,11 +5,6 @@ module PreCommit
       @staged_files ||= `git diff --cached --name-only --diff-filter=ACM`.split
     end
 
-    def self.new_files(*dirs)
-      @new_files ||= {}
-      @new_files[dirs.join(' ')] ||= `git status --short #{dirs.join(' ')} | grep ^A | xargs`.chomp.split("A ").join(" ")
-    end
-
     def self.grep
       grep_version = `grep --version | head -n 1 | sed -e 's/^[^0-9.]*\([0-9.]*\)$/\1/'`
       if grep_version =~ /FreeBSD/
