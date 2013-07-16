6b9e5cc902b2ffb1989ccd287b4204f43ec2433c
Benjamin Quorning <bquorning@zendesk.com>
2013-07-16 22:25:25 +0200

    Faster check for trailing whitespace

--- lib/pre-commit/checks/whitespace_check.rb
+++ lib/pre-commit/checks/whitespace_check.rb
@@ -1,9 +1,7 @@
 module PreCommit
   class WhiteSpaceCheck
-    WHITESPACE_SCRIPT_PATH = File.expand_path("../../support/whitespace/whitespace", __FILE__)
-
     def self.call(_)
-      errors = `sh #{WHITESPACE_SCRIPT_PATH}`
+      errors = `git diff-index --check --cached head --`
       return if $?.success?
       errors
     end
