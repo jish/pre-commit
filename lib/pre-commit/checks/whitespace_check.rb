b803e8cbd7a424f04536f90fe516bee538a09b8b
grosser <grosser.michael@gmail.com>
2013-07-17 14:22:39 -0700

    bring back empty object comment

--- lib/pre-commit/checks/whitespace_check.rb
+++ lib/pre-commit/checks/whitespace_check.rb
@@ -4,6 +4,7 @@ module PreCommit
       errors = `git diff-index --check --cached head -- 2>&1`
       return if $?.success?
 
+      # Initial commit: diff against the empty tree object
       if errors =~ /fatal: bad revision 'head'/
         errors = `git diff-index --check --cached 4b825dc642cb6eb9a060e54bf8d69288fbee4904 -- 2>&1`
         return if $?.success?
