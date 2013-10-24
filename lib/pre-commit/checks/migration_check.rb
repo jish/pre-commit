fc215d9fd4c13420a59f6c50f9a05a2fae3a6600
grosser <grosser.michael@gmail.com>
2013-10-23 20:13:31 -0700

    check if version was added to schema file

--- lib/pre-commit/checks/migration_check.rb
+++ lib/pre-commit/checks/migration_check.rb
@@ -2,30 +2,31 @@ module PreCommit
   class MigrationCheck
     class << self
       def call(staged_files)
-        migration_present = migration_file_present?(staged_files)
-        schema_change = schema_file_present?(staged_files)
+        migration_files = migration_files(staged_files)
+        schema_files = schema_files(staged_files)
 
-        if migration_present && !schema_change
+        if migration_files.any? && schema_files.none?
           "It looks like you're adding a migration, but did not update the schema file"
-        elsif schema_change && !migration_present
+        elsif migration_files.none? && schema_files.any?
           "You're trying to change the schema without adding a migration file"
+        elsif migration_files.any? && schema_files.any?
+          versions = migration_files.map { |f| f[/\d+/] }
+          schema = schema_files.map { |f| File.read(f) }.join
+          missing_versions = versions.select { |version| !schema.include?(version) }
+          if missing_versions.any?
+            "You did not add the schema versions for #{versions.join(', ')} to #{schema_files.join(' or ')}"
+          end
         end
       end
 
       private
 
-      def migration_file_present?(staged_files)
-        staged_files.any? { |file| file =~ /db\/migrate\/.*\.rb/ }
+      def migration_files(staged_files)
+        staged_files.grep(/db\/migrate\/.*\.rb/)
       end
 
-      def schema_file_present?(staged_files)
-        staged_files.any? do |file|
-          basename = File.basename(file)
-
-          [/schema\.rb/i, /structure.*\.sql/].any? do |regex|
-            basename =~ regex
-          end
-        end
+      def schema_files(staged_files)
+        staged_files.select { |f| File.basename(f) =~ /schema\.rb|structure.*\.sql/ }
       end
     end
   end
