52c9c10b46b8ddefd9d29063e5d49268e97f3646
Josh Lubaway <josh.lubaway@gmail.com>
2013-12-19 13:17:41 -0800

    Add an option to install a different hook template.

--- lib/pre-commit/cli.rb
+++ lib/pre-commit/cli.rb
@@ -4,11 +4,23 @@ module PreCommit
   class Cli
 
     PRE_COMMIT_HOOK_PATH = '.git/hooks/pre-commit'
+    TEMPLATE_DIR = File.expand_path("../support/templates/", __FILE__)
+    TEMPLATE_MAP = { "--manual" => "manual" }
 
-    def install
-      hook = File.expand_path("../support/templates/pre-commit-hook", __FILE__)
+    def install(template_key = nil)
+      hook = template_file(template_key)
       FileUtils.cp(hook, PRE_COMMIT_HOOK_PATH)
       FileUtils.chmod(0755, PRE_COMMIT_HOOK_PATH)
+    end
+
+    def template_file(key)
+      template = hook_template(key)
+      File.exists?(template) ? template : hook_template("default")
+    end
+
+    def hook_template(key)
+      template = TEMPLATE_MAP[key] || "pre-commit-hook"
+      File.join(TEMPLATE_DIR, template)
     end
 
   end
