c3d5583c3745dbeb114f7a20bbb6ae50e2dd06e3
Michal Papis <mpapis@gmail.com>
2013-12-25 05:12:42 +0100

    add cli action show

--- lib/pre-commit/cli.rb
+++ lib/pre-commit/cli.rb
@@ -1,4 +1,5 @@
 require 'fileutils'
+require 'pre-commit/configuration'
 
 module PreCommit
 
@@ -9,13 +10,26 @@ module PreCommit
     PRE_COMMIT_HOOK_PATH = '.git/hooks/pre-commit'
     TEMPLATE_DIR = File.expand_path("../support/templates/", __FILE__)
 
-    attr_reader :templates
-
-    def initialize
-      @templates = load_templates
+    def initialize(*args)
+      @args = args
     end
 
-    def install(key = nil)
+    def execute()
+      action = @args.shift or 'help'
+      action = "execute_#{action}".to_sym
+      if respond_to?(action)
+      then send(action, *@args)
+      else execute_help(action, *@args)
+      end
+    end
+
+    def execute_help(*args)
+      warn "Unknown parameters: #{args * " "}" unless args.empty?
+      warn "Usage: pre-commit [install|show]"
+      args.empty? # return status, it's ok if user requested help
+    end
+
+    def execute_install(key = nil, *args)
       key ||= "default"
       hook = templates[key.sub(/^--/, "")]
 
@@ -23,17 +37,36 @@ module PreCommit
 
       FileUtils.cp(hook, PRE_COMMIT_HOOK_PATH)
       FileUtils.chmod(0755, PRE_COMMIT_HOOK_PATH)
+    rescue PreCommit::TemplateNotFound => e
+      warn e.message
+      false
+    else
+      puts "Installed #{hook} to #{PreCommit::Cli::PRE_COMMIT_HOOK_PATH}"
+      true
     end
 
-    private
+    def execute_show(*args)
+      puts "Default checks: #{config.get_arr(:checks).join(" ")}"
+      puts "Enabled checks: #{config.get_combined(:checks).join(" ")}"
+      puts "Default warnings: #{config.get_arr(:warnings).join(" ")}"
+      puts "Enabled warnings: #{config.get_combined(:warnings).join(" ")}"
+      true
+    end
 
-    def load_templates
+  private
+
+    def config
+      @config ||= PreCommit::Configuration.new(Pluginator.find('pre_commit'))
+    end
+
+    def templates
+      return @templates if @templates
       pattern = File.join(TEMPLATE_DIR, "*_hook")
 
+      @templates =
       Dir.glob(pattern).inject({}) do |hash, file|
         key = file.match(/\/([^\/]+?)_hook$/)[1]
         hash[key] = file
-
         hash
       end
     end
--- test/unit/cli_test.rb
+++ test/unit/cli_test.rb
@@ -1,41 +1,79 @@
 require 'minitest_helper'
 require 'pre-commit/cli'
+require 'stringio'
 
 describe PreCommit::Cli do
 
   before do
     create_temp_dir
+    start_git
     FileUtils.mkdir_p(".git/hooks")
+    $stderr = StringIO.new
+    $stdout = StringIO.new
   end
-  after(&:destroy_temp_dir)
+  after do
+    destroy_temp_dir
+    $stderr = STDERR
+    $stdout = STDOUT
+  end
 
-  let(:cli) { PreCommit::Cli.new }
+  subject { PreCommit::Cli }
   let(:hook) { PreCommit::Cli::PRE_COMMIT_HOOK_PATH }
+
+  it "shows help" do
+    cli = subject.new('help')
+    cli.execute.must_equal(true)
+    $stderr.string.must_match(/Usage:/)
+    $stdout.string.must_equal('')
+  end
+
+  it "shows configuration" do
+    cli = subject.new('show')
+    cli.execute.must_equal(true)
+    $stderr.string.must_equal('')
+    $stdout.string.gsub(/\s+\n/,"\n").must_equal(<<-EXPECTED)
+Default checks: white_space console_log debugger pry tabs jshint migrations merge_conflict local nb_space
+Enabled checks: white_space console_log debugger pry tabs jshint migrations merge_conflict local nb_space
+Default warnings:
+Enabled warnings:
+EXPECTED
+  end
 
   it "intalls the hook" do
     File.exists?(hook).must_equal false
-    cli.install
+    cli = subject.new('install')
+    cli.execute.must_equal(true)
     File.exists?(hook).must_equal true
-    File.read(hook).must_equal File.read(cli.templates["default"])
+    File.read(hook).must_equal File.read(cli.send(:templates)["default"])
+    $stderr.string.must_equal('')
+    $stdout.string.must_match(/Installed .*lib\/pre-commit\/support\/templates\/default_hook to #{hook}\n/)
   end
 
   it "installs other hook templates" do
     File.exists?(hook).must_equal false
-    cli.install("--manual")
+    cli = subject.new('install', '--manual')
+    cli.execute.must_equal(true)
     File.exists?(hook).must_equal true
-    File.read(hook).must_equal File.read(cli.templates["manual"])
+    File.read(hook).must_equal File.read(cli.send(:templates)["manual"])
+    $stderr.string.must_equal('')
+    $stdout.string.must_match(/Installed .*lib\/pre-commit\/support\/templates\/manual_hook to #{hook}\n/)
   end
 
   it "installs the default hook when passed --automatic" do
     File.exists?(hook).must_equal false
-    cli.install("--automatic")
+    cli = subject.new('install', '--automatic')
+    cli.execute.must_equal(true)
     File.exists?(hook).must_equal true
-    File.read(hook).must_equal File.read(cli.templates["default"])
+    File.read(hook).must_equal File.read(cli.send(:templates)["default"])
+    $stderr.string.must_equal('')
+    $stdout.string.must_match(/Installed .*lib\/pre-commit\/support\/templates\/automatic_hook to #{hook}\n/)
   end
 
   it "handles missing templates" do
     File.exists?(hook).must_equal false
-    -> { cli.install("--not-found") }.must_raise(PreCommit::TemplateNotFound)
+    subject.new('install', '--not-found').execute.must_equal(false)
+    $stderr.string.must_match(/Could not find template/)
+    $stdout.string.must_equal('')
   end
 
 end
