7b47ae62f21fe64b6b6a391b09087e6dcd2cd001
Michal Papis <mpapis@gmail.com>
2013-12-25 12:09:13 +0100

    add support for system and global yaml config files

--- lib/plugins/pre_commit/configuration/providers/yaml.rb
+++ lib/plugins/pre_commit/configuration/providers/yaml.rb
@@ -13,12 +13,31 @@ module PreCommit
           config[name]
         end
 
+      private
+
         def config
-          return Hash.new unless File.exist?(file_name)
-          @config ||= YAML.load_file(file_name) || Hash.new
+          return @config if @config
+          @config = {}
+          read_config(system_file)
+          read_config(global_file)
+          read_config(local_file)
+          @config
         end
 
-        def file_name
+        def read_config(path)
+          content = YAML.load_file(path) if File.exist?(path)
+          @config.merge!(content) if content
+        end
+
+        def system_file
+          @system_file ||= '/etc/pre_commit.yml'
+        end
+
+        def global_file
+          @global_file ||= File.join(ENV['HOME'], '.pre_commit.yml')
+        end
+
+        def local_file
           File.join(top_level, 'config', 'pre_commit.yml')
         end
 
--- test/unit/configuration/providers/yaml_test.rb
+++ test/unit/configuration/providers/yaml_test.rb
@@ -14,22 +14,78 @@ describe PreCommit::Configuration::Providers::Yaml do
     before do
       create_temp_dir
       start_git
-      Dir.mkdir("config")
-      File.open("config/pre_commit.yml", "w") do |file|
-        file.write <<-DATA
----
-:test1: 5
-:test3: 6
-DATA
-      end
+      @system_file = "etc_pre_commit.yml"
+      @global_file = ".pre_commit.yml"
+      preprare_file(@system_file, SYSTEM_DATA)
     end
     after(&:destroy_temp_dir)
 
-    it "reads values" do
+    it "reads system values" do
       example = subject.new
-      example[:test1].must_equal(5)
-      example[:test2].must_equal(nil)
-      example[:test3].must_equal(6)
+      example.instance_variable_set(:@system_file, @system_file)
+      example.instance_variable_set(:@global_file, @global_file)
+      example[:test1].must_equal(1)
+      example[:test2].must_equal(2)
+      example[:test3].must_equal(3)
+      example[:test4].must_equal(nil)
+    end
+
+    it "reads system and global values" do
+      preprare_file(@global_file, GLOBAL_DATA)
+      example = subject.new
+      example.instance_variable_set(:@system_file, @system_file)
+      example.instance_variable_set(:@global_file, @global_file)
+      example[:test1].must_equal(4)
+      example[:test2].must_equal(5)
+      example[:test3].must_equal(3)
+      example[:test4].must_equal(nil)
+    end
+
+    it "reads system and local values" do
+      Dir.mkdir("config")
+      preprare_file("config/pre_commit.yml", LOCAL_DATA)
+      example = subject.new
+      example.instance_variable_set(:@system_file, @system_file)
+      example.instance_variable_set(:@global_file, @global_file)
+      example[:test1].must_equal(6)
+      example[:test2].must_equal(2)
+      example[:test3].must_equal(3)
+      example[:test4].must_equal(nil)
+    end
+    it "reads system and local values" do
+      preprare_file(@global_file, GLOBAL_DATA)
+      Dir.mkdir("config")
+      preprare_file("config/pre_commit.yml", LOCAL_DATA)
+      example = subject.new
+      example.instance_variable_set(:@system_file, @system_file)
+      example.instance_variable_set(:@global_file, @global_file)
+      example[:test1].must_equal(6)
+      example[:test2].must_equal(5)
+      example[:test3].must_equal(3)
+      example[:test4].must_equal(nil)
     end
   end
+
+  def preprare_file(name, content)
+    File.open(name, "w") {|file| file.write(content) }
+  end
+
+  SYSTEM_DATA = <<-DATA
+---
+:test1: 1
+:test2: 2
+:test3: 3
+DATA
+
+  GLOBAL_DATA = <<-DATA
+---
+:test1: 4
+:test2: 5
+DATA
+
+  LOCAL_DATA = <<-DATA
+---
+:test1: 6
+DATA
+
 end
