e87cbd81a46f5473fb30c8137c3fc1270259ac66
Michal Papis <mpapis@gmail.com>
2014-10-07 21:09:15 +0200

    group code in staged_files_test

--- test/unit/pre-commit/utils/staged_files_test.rb
+++ test/unit/pre-commit/utils/staged_files_test.rb
@@ -11,57 +11,65 @@ describe PreCommit::Utils::StagedFiles do
     Object.new.send(:extend, PreCommit::Utils::StagedFiles)
   end
 
-  it "finds staged files" do
-    write("test.rb", "\t\t Muahaha\n\n\n")
-    sh "git add -A"
-    subject.staged_files.must_equal(['test.rb'])
-  end
+  describe :staged_files do
 
-  it "filters out binary files" do
-    write("test.rb", (1..50).map(&:chr).join)
-    sh "git add -A"
-    subject.staged_files.must_equal([])
-  end
+    it "finds staged files" do
+      write("test.rb", "\t\t Muahaha\n\n\n")
+      sh "git add -A"
+      subject.staged_files.must_equal(['test.rb'])
+    end
+
+    it "filters out binary files" do
+      write("test.rb", (1..50).map(&:chr).join)
+      sh "git add -A"
+      subject.staged_files.must_equal([])
+    end
 
 
-  it "has empty list for no changes" do
-    subject.staged_files.must_equal([])
-  end
+    it "has empty list for no changes" do
+      subject.staged_files.must_equal([])
+    end
 
-  it "sets staged files" do
-    subject.staged_files.must_equal([])
-    subject.set_staged_files("some_file", "another_file")
-    subject.staged_files.must_equal(["some_file", "another_file"])
-  end
+    it "sets staged files" do
+      subject.staged_files.must_equal([])
+      subject.set_staged_files("some_file", "another_file")
+      subject.staged_files.must_equal(["some_file", "another_file"])
+    end
 
-  it "does not include links to nowhere" do
-    write("something.rb", "class Something; end")
-    write("nowhere.rb", "")
-    FileUtils.ln_s("nowhere.rb", "link_to_nowhere.rb")
-    FileUtils.rm("nowhere.rb")
-    system("git", "add", "-A")
-    subject.staged_files.must_equal ["something.rb"]
-  end
+    it "does not include links to nowhere" do
+      write("something.rb", "class Something; end")
+      write("nowhere.rb", "")
+      FileUtils.ln_s("nowhere.rb", "link_to_nowhere.rb")
+      FileUtils.rm("nowhere.rb")
+      system("git", "add", "-A")
+      subject.staged_files.must_equal ["something.rb"]
+    end
 
-  it "builds ignores file path" do
-    subject.send(:repo_ignores_file).must_match(%r{.*/.pre_commit.ignore$})
-  end
+  end # :staged_files
 
-  it "does not fail without ignore file" do
-    subject.send(:repo_ignores).must_equal []
-  end
+  describe :repo_ignores do
 
-  it "does read ignores file" do
-    write(".pre_commit.ignore", "file.rb\n")
-    subject.send(:repo_ignores).must_equal ["file.rb"]
-  end
+    it "builds ignores file path" do
+      subject.send(:repo_ignores_file).must_match(%r{.*/.pre_commit.ignore$})
+    end
 
-  it "it does exclude links from ignore file" do
-    write(".pre_commit.ignore", "file.rb\n")
-    write("something.rb", "")
-    write("file.rb", "")
-    system("git", "add", "-A")
-    subject.staged_files.sort.must_equal [".pre_commit.ignore", "something.rb"].sort
-  end
+    it "does not fail without ignore file" do
+      subject.send(:repo_ignores).must_equal []
+    end
+
+    it "does read ignores file" do
+      write(".pre_commit.ignore", "file.rb\n")
+      subject.send(:repo_ignores).must_equal ["file.rb"]
+    end
+
+    it "it does exclude links from ignore file" do
+      write(".pre_commit.ignore", "file.rb\n")
+      write("something.rb", "")
+      write("file.rb", "")
+      system("git", "add", "-A")
+      subject.staged_files.sort.must_equal [".pre_commit.ignore", "something.rb"].sort
+    end
+
+  end # :repo_ignores
 
 end
