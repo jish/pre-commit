ec99266a8828d7cb2feea806048681e9e870dda9
Josh Lubaway <josh.lubaway@gmail.com>
2017-10-18 21:56:47 -0700

    Document the CI check

--- lib/plugins/pre_commit/checks/ci.rb
+++ lib/plugins/pre_commit/checks/ci.rb
@@ -2,20 +2,25 @@ require 'pre-commit/checks/plugin'
 
 module PreCommit
   module Checks
+    ##
+    # The CI check will run `rake pre_commmit:ci` before the commit and check
+    # its exit code. If the task runs successfully, the commit will proceed.
+    # If it fails, the commit will be aborted.
+    #
     class Ci < Plugin
       CI_TASK_NAME = 'pre_commit:ci'
-
-      def call(_)
-        return if system("rake #{Ci::CI_TASK_NAME} --silent")
-        PreCommit::ErrorList.new(
-          "your test suite has failed, for the full output run `#{CI_TASK_NAME}`"
-        )
-      end
 
       def self.description
         "Runs 'rake #{CI_TASK_NAME} --silent'"
       end
 
+      def call(_)
+        return if system("rake", CI_TASK_NAME, "--silent")
+
+        PreCommit::ErrorList.new(
+          "your test suite has failed, for the full output run `#{CI_TASK_NAME}`"
+        )
+      end
     end
   end
 end
